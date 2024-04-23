f48cbc33b41234f4e715eb1bd9617ac0d835186a
ventiv 111 <1841013744@qq.com>
2024-04-23 07:51:59 +0000

    1

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,26 @@ import 'dart:io';
 
 import 'package:args/args.dart';
 
-const lineNumber = 'line-number';
+const flagLineNumber = 'line-number';
 
 void main(List<String> arguments) {
-  exitCode = 0; // Presume success
-  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+  exitCode = 0; // 假设成功
+  final parser = ArgParser()..addFlag(flagLineNumber, negatable: false, abbr: 'n');
 
   ArgResults argResults = parser.parse(arguments);
   final paths = argResults.rest;
 
-  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
+  dcat(paths, showLineNumbers: argResults[flagLineNumber] as bool);
 }
 
 Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
   if (paths.isEmpty) {
-    // No files provided as arguments. Read from stdin and print each line.
+    // 没有提供文件作为参数。从stdin读取并打印每一行。
     await stdin.pipe(stdout);
   } else {
     for (final path in paths) {
       var lineNumber = 1;
-      final lines = utf8.decoder
-          .bind(File(path).openRead())
-          .transform(const LineSplitter());
+      final lines = utf8.decoder.bind(File(path).openRead()).transform(const LineSplitter());
       try {
         await for (final line in lines) {
           if (showLineNumbers) {
@@ -41,7 +39,7 @@ Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
 
 Future<void> _handleError(String path) async {
   if (await FileSystemEntity.isDirectory(path)) {
-    stderr.writeln('error: $path is a directory');
+    stderr.writeln('错误：$path 是一个目录');
   } else {
     exitCode = 2;
   }
