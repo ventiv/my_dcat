5ea452fe75933b1b89e107150851f2be491d26dd
ventiv 111 <1841013744@qq.com>
2024-04-23 07:42:33 +0000

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,48 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'dart:io';
 
+import 'package:args/args.dart';
 
-void main() async {
-  await printPackageInformation('http');
-  print('');
-  await printPackageInformation('path');
+const lineNumber = 'line-number';
+
+void main(List<String> arguments) {
+  exitCode = 0; // Presume success
+  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+
+  ArgResults argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+
+  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
 }
 
-Future<void> printPackageInformation(String packageName) async {
-  final PackageInfo packageInfo;
-
-  try {
-    packageInfo = await getPackage(packageName);
-  } on PackageRetrievalException catch (e) {
-    print(e);
-    return;
-  }
-
-  print('Information about the $packageName package:');
-  print('Latest version: ${packageInfo.latestVersion}');
-  print('Description: ${packageInfo.description}');
-  print('Publisher: ${packageInfo.publisher}');
-
-  final repository = packageInfo.repository;
-  if (repository != null) {
-    print('Repository: $repository');
-  }
-}
-
-Future<PackageInfo> getPackage(String packageName) async {
-  final packageUrl = Uri.https('dart.dev', '/f/packages/$packageName.json');
-  final packageResponse = await http.get(packageUrl);
-
-  // If the request didn't succeed, throw an exception
-  if (packageResponse.statusCode != 200) {
-    throw PackageRetrievalException(
-      packageName: packageName,
-      statusCode: packageResponse.statusCode,
-    );
-  }
-
-  final packageJson = json.decode(packageResponse.body) as Map<String, dynamic>;
-
-  return PackageInfo.fromJson(packageJson);
-}
-
-class PackageInfo {
-  final String name;
-  final String latestVersion;
-  final String description;
-  final String publisher;
-  final Uri? repository;
-
-  PackageInfo({
-    required this.name,
-    required this.latestVersion,
-    required this.description,
-    required this.publisher,
-    this.repository,
-  });
-
-  factory PackageInfo.fromJson(Map<String, dynamic> json) {
-    final repository = json['repository'] as String?;
-
-    return PackageInfo(
-      name: json['name'] as String,
-      latestVersion: json['latestVersion'] as String,
-      description: json['description'] as String,
-      publisher: json['publisher'] as String,
-      repository: repository != null ? Uri.tryParse(repository) : null,
-    );
-  }
-}
-
-class PackageRetrievalException implements Exception {
-  final String packageName;
-  final int? statusCode;
-
-  PackageRetrievalException({required this.packageName, this.statusCode});
-
-  @override
-  String toString() {
-    final buf = StringBuffer();
-    buf.write('Failed to retrieve package:$packageName information');
-
-    if (statusCode != null) {
-      buf.write(' with a status code of $statusCode');
+Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
+  if (paths.isEmpty) {
+    // No files provided as arguments. Read from stdin and print each line.
+    await stdin.pipe(stdout);
+  } else {
+    for (final path in paths) {
+      var lineNumber = 1;
+      final lines = utf8.decoder
+          .bind(File(path).openRead())
+          .transform(const LineSplitter());
+      try {
+        await for (final line in lines) {
+          if (showLineNumbers) {
+            stdout.write('${lineNumber++} ');
+          }
+          stdout.writeln(line);
+        }
+      } catch (_) {
+        await _handleError(path);
+      }
     }
+  }
+}
 
-    buf.write('!');
-    return buf.toString();
+Future<void> _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+  } else {
+    exitCode = 2;
   }
 }
